05eba9c7c0e2466948dd62fd7d0fef906d045272
Brijal Kansara <brijalkansara091@gmail.com>
2024-01-27 14:36:20 +0530

    [ADD]Fonts and React-Router-Dom

--- Frontend/HomeNeeds/src/App.tsx
+++ Frontend/HomeNeeds/src/App.tsx
@@ -1,18 +1,15 @@
-import React from 'react'
 import Header from './Modules/shared/Header/Header';
-import { Container } from 'react-bootstrap';
 import Footer from './Modules/shared/Footer/Footer';
 import Home from "../src/Modules/Home/Home";
+import { Routes ,Route} from 'react-router-dom';
 
 export default function App() {
   return (
     <>
     <Header />
-     <main className='py-3'>
-      <Container>
-        <Home/>
-      </Container>
-     </main>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+    </Routes>
      <Footer/>
     </>
   )
--- Frontend/HomeNeeds/src/Modules/Home/Home.tsx
+++ Frontend/HomeNeeds/src/Modules/Home/Home.tsx
@@ -1,21 +1,32 @@
-import React from 'react'
-import {Row, Col} from 'react-bootstrap';
-import products from '../../products';
-import ProductList from '../ProductList/ProductList';
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import products from "../../products";
+import ProductList from "../ProductList/ProductList";
+import { Container } from "react-bootstrap";
 
 export default function Home() {
   return (
-    <>
-     <h1>Latest Products</h1> 
-     <Row>
-      {products.map((product:any)=>{
-        return(
-          <Col sm={12} md={6} lg={4} xl={3} key={product.id}>
-            <ProductList product={product}/>
-          </Col>
-        )
-      })}
-     </Row>
-    </>
-  )
+    <main className="py-3">
+      <Container>
+        <h1>Latest Products</h1>
+        <Row>
+          {products.map((product: any) => {
+            return (
+              <Col sm={12} md={6} lg={4} xl={3} key={product.id}>
+                <ProductList product={product} />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </main>
+  );
+}
+
+{
+  /* <main className='py-3'>
+      <Container>
+        <Home/>
+      </Container>
+     </main> */
 }
